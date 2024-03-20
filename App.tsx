daebdc7279a74c75fdbf779059be85015adea96e
Jon Moore <jonevan3@gmail.com>
2024-03-20 15:58:48 -0600

    install node

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>I can see the TExt!</Text>
       <StatusBar style="auto" />
     </View>
   );
